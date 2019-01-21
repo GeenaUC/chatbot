9ff9fd279dc6f5c651a22605d409c385c9dba948
Geena Sim <g.geenasim@gmail.com>
2019-01-21 12:30:35 +0700

    setup web service

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = process.env.POST || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req,res) => {
+    res.send('Hello');
+})
+
+app.post('/webhook', (req,res) => {
+    console.log('---- /webhook ----');
+    let body = req.body;
+    let respone = {
+        status: 'ok',
+        body: body
+    };
+    console.log('===> Body');
+    console.log(body);
+    res.send(respone);
+})
+
+app.listen(3000, () => {
+    console.log(`run on port : ${port}`);
+})
